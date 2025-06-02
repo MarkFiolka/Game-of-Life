407887f3ece0bd2a5fb1be7fe23364b72835b775
Mark Fiolka <mfi@cmc-kiesel.de>
2025-06-02 16:23:42 +0200

    initial commit

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField] private Tilemap currentState;
+    [SerializeField] private Tilemap nextState;
+    [SerializeField] private Tile aliveTile;
+    [SerializeField] private Tile deadTile;
+    [SerializeField] private Pattern pattern;
+    [SerializeField] private float updateInterval = 0.05f;
+
+    private void Start()
+    {
+        SetPattern(pattern);
+    }
+
+    private void SetPattern(Pattern pattern)
+    {
+        Clear();
+
+        Vector2Int center = pattern.GetCenter();
+        
+        for (int i = 0; i < pattern.cells.Length; i++)
+        {
+            Vector2Int cell = pattern.cells[i] - center;
+            currentState.SetTile((Vector3Int)cell, aliveTile);
+        }
+    }
+
+    private void Clear()
+    {
+        currentState.ClearAllTiles();
+        nextState.ClearAllTiles();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Simulate());
+    }
+    
+    private IEnumerator Simulate()
+    {
+        while (enabled)
+        {
+            UpdateState();
+            yield return new WaitForSeconds(updateInterval);
+        }
+    }
+
+    private void UpdateState()
+    {
+        
+    }
+}
